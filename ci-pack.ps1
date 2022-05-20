323780ec512e672a746c6eef31007b81bf0d84e6
Brett Hall <bhall@wyatt.com>
2022-05-20 16:46:58 -0700

    Fixed pack script

--- ci-pack.ps1
+++ ci-pack.ps1
@@ -1,2 +1,2 @@
 # This script exists because putting the following line directly into a script block in gitlab-ci.yml doesn't work.
-.\runBuild.ps1 -Target pack-utils -PackVersion $env:CI_COMMIT_TAG
+.\runBuild.ps1 -Target pack -PackVersion $env:CI_COMMIT_TAG
