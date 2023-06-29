2558ede346c5c8b31b8657223cf09d482b2952cf
Brett Hall <bhall@wyatt.com>
2023-06-29 16:09:27 -0700

    Messages for tests are now sent before timing begins

--- PerformanceTest/Program.fs
+++ PerformanceTest/Program.fs
@@ -5,51 +5,56 @@ open Microsoft.FSharp.Core
 
 type GetInfo = GetInfo
 
+type StartMsg = {
+    ready: System.Threading.Tasks.TaskCompletionSource<unit>
+    start: System.Threading.Tasks.TaskCompletionSource<unit>
+}
+
 type TestMsg = {
     index: int
     stop: Option<System.Threading.Tasks.TaskCompletionSource<unit>>
 }
 
-type Sender(recv: IActorRef<TestMsg>, numMessages: int) =
-    
-    inherit Akka.Actor.UntypedActor ()
-        
-    let ctx = Akka.Actor.UntypedActor.Context :> Akka.Actor.IActorContext
-    let mutable stash = Unchecked.defaultof<Akka.Actor.IStash>
-
-    let start = System.DateTime.Now
-    let mutable finish = Unchecked.defaultof<System.DateTime>
-    
-    do 
-        for i in 1..numMessages do
-            recv <! {index = i; stop = None}
-            
-    let mutable nextIndex = 1
-    
-    override _.OnReceive (msg: obj) =
-        match msg with
-        | :? TestMsg as testMsg ->
-            if testMsg.index = nextIndex then
-                if nextIndex = numMessages then
-                    finish <- System.DateTime.Now
-                    nextIndex <- -1
-                else
-                    nextIndex <- nextIndex + 1
-                stash.UnstashAll ()
-            else
-                stash.Stash ()
-        | :? GetInfo ->
-            if nextIndex  = -1 then 
-                typed ctx.Sender <! (finish - start)
-            else
-                stash.Stash ()
-        | _ ->
-            ()
-    
-    interface Akka.Actor.IWithUnboundedStash with
-        member _.Stash
-            with get () = stash
-            and set newStash = stash <- newStash
+// type Sender(recv: IActorRef<TestMsg>, numMessages: int) =
+//     
+//     inherit Akka.Actor.UntypedActor ()
+//         
+//     let ctx = Akka.Actor.UntypedActor.Context :> Akka.Actor.IActorContext
+//     let mutable stash = Unchecked.defaultof<Akka.Actor.IStash>
+//
+//     let start = System.DateTime.Now
+//     let mutable finish = Unchecked.defaultof<System.DateTime>
+//     
+//     do 
+//         for i in 1..numMessages do
+//             recv <! {index = i; stop = None}
+//             
+//     let mutable nextIndex = 1
+//     
+//     override _.OnReceive (msg: obj) =
+//         match msg with
+//         | :? TestMsg as testMsg ->
+//             if testMsg.index = nextIndex then
+//                 if nextIndex = numMessages then
+//                     finish <- System.DateTime.Now
+//                     nextIndex <- -1
+//                 else
+//                     nextIndex <- nextIndex + 1
+//                 stash.UnstashAll ()
+//             else
+//                 stash.Stash ()
+//         | :? GetInfo ->
+//             if nextIndex  = -1 then 
+//                 typed ctx.Sender <! (finish - start)
+//             else
+//                 stash.Stash ()
+//         | _ ->
+//             ()
+//     
+//     interface Akka.Actor.IWithUnboundedStash with
+//         member _.Stash
+//             with get () = stash
+//             and set newStash = stash <- newStash
     
 module WAkkaTest =
     open WAkka.Common
@@ -64,7 +69,12 @@ module WAkkaTest =
                     msg.stop.Value.SetResult ()
                 return! handle ()
             }
-            handle ()
+            actor {
+                let! msg = Receive.Only<StartMsg>()
+                msg.ready.SetResult ()
+                msg.start.Task.Wait()
+                return! handle ()
+            }
         ))
 
 module WAkkaWhileTest =
@@ -76,6 +86,9 @@ module WAkkaWhileTest =
         spawn parent Props.Anonymous (notPersisted (
             let mutable state = 0
             actor {
+                let! msg = Receive.Only<StartMsg>()
+                msg.ready.SetResult ()
+                msg.start.Task.Wait()
                 while true do
                     let! msg = Receive.Only<TestMsg>()
                     state <- state + 1
@@ -96,7 +109,12 @@ module WAkkaSmTest =
                     msg.stop.Value.SetResult ()
                 return! handle ()
             }
-            handle ()
+            smActor {
+                let! msg = receive()
+                msg.ready.SetResult ()
+                msg.start.Task.Wait()
+                return! handle ()
+            }
         )
 
 module AkkaTest =
@@ -112,6 +130,9 @@ module AkkaTest =
             | :? TestMsg as testMsg -> 
                 if testMsg.stop.IsSome then
                     testMsg.stop.Value.SetResult ()
+            | :? StartMsg as msg ->
+                msg.ready.SetResult ()
+                msg.start.Task.Wait()
             | _ ->
                 ()
             
@@ -135,6 +156,9 @@ module AkkaWithStateTest =
                 if testMsg.stop.IsSome then
                     testMsg.stop.Value.SetResult ()
                     state <- 0
+            | :? StartMsg as msg ->
+                msg.ready.SetResult ()
+                msg.start.Task.Wait()
             | _ ->
                 ()
             
@@ -146,24 +170,47 @@ module AkklingTest =
     
     let makeActor parent = 
             Spawn.spawnAnonymous parent (Props.props(fun _ctx ->
-                become (fun (msg:TestMsg) -> 
-                    if msg.stop.IsSome then
-                        msg.stop.Value.SetResult ()
+                let handle (msg:obj) = 
+                    match msg with
+                    | :? TestMsg as msg ->  
+                        if msg.stop.IsSome then
+                            msg.stop.Value.SetResult ()
+                    | _ -> ()
                     ignored ()
-                )
-            ))            
+                become (fun (msg: obj) ->
+                    match msg with
+                    | :? StartMsg as msg ->  
+                        msg.ready.SetResult()
+                        msg.start.Task.Wait()
+                        become handle
+                    | _ ->
+                        ignored()
+                )                
+            ))
 
 module AkklingWithStateTest =
     
     let makeActor parent = 
             Spawn.spawnAnonymous parent (Props.props(fun _ctx ->
-                let rec loop value (msg:TestMsg) = 
-                    if msg.stop.IsSome then
-                        msg.stop.Value.SetResult ()
+                let rec loop value (msg:obj) = 
+                    match msg with
+                    | :? TestMsg as msg ->  
+                        if msg.stop.IsSome then
+                            msg.stop.Value.SetResult ()
+                            become (loop 0)
+                        else
+                            become (loop (value + 1))
+                    | _ ->
+                        ignored()
+                become (fun (msg: obj) ->
+                    match msg with
+                    | :? StartMsg as msg ->  
+                        msg.ready.SetResult()
+                        msg.start.Task.Wait()
                         become (loop 0)
-                    else
-                        become (loop (value + 1))
-                become (loop 0)
+                    | _ ->
+                        ignored ()
+                )                
             ))
             
 type Args =
@@ -188,91 +235,127 @@ let args =
         System.Environment.Exit 1
         Unchecked.defaultof<_>
 
+type StartStop = {
+    start: System.Threading.Tasks.TaskCompletionSource<unit>
+    stop: System.Threading.Tasks.TaskCompletionSource<unit>
+}
+
 [<BenchmarkDotNet.Attributes.MemoryDiagnoser>]
 type ActorBenchmarks () =
     
-    let numMessages = 10000
+    let numMessages = 3000000
     let msgs = [|
         for i in 1..numMessages do
             {index = i; stop = None}
     |]
     
     let mutable sys = Unchecked.defaultof<_>
-    let mutable akkaActor = Unchecked.defaultof<_>
-    let mutable akkaWithStateActor = Unchecked.defaultof<_>
-    let mutable akklingActor = Unchecked.defaultof<_>
-    let mutable akklingWithStateActor = Unchecked.defaultof<_>
-    let mutable wakkaActor = Unchecked.defaultof<_>
-    let mutable wakkaWhileActor = Unchecked.defaultof<_>
-    let mutable wakkaSmActor = Unchecked.defaultof<_>
+    let mutable startStop = Unchecked.defaultof<StartStop>
     
-    let runTest testActor =
+    let prepTest (testActor: IActorRef<obj>) =
+        let ready = System.Threading.Tasks.TaskCompletionSource<unit>()
+        let start = System.Threading.Tasks.TaskCompletionSource<unit>()
+        testActor <! {start = start; ready = ready}
         for msg in msgs do
-            testActor <! msg
-        let tcs = System.Threading.Tasks.TaskCompletionSource<unit>()
-        testActor <! {index = 0; stop = Some tcs}
-        tcs.Task.Wait()
-
+            (retype testActor) <! msg
+        let stop = System.Threading.Tasks.TaskCompletionSource<unit>()
+        (retype testActor) <! {index = 0; stop = Some stop}
+        ready.Task.Wait()
+        {start = start; stop = stop}
+        
     [<BenchmarkDotNet.Attributes.GlobalSetup>]
     member _.Setup () =
         sys <- System.create "perf-test" (Configuration.defaultConfig ())
-        akkaActor <- AkkaTest.makeActor sys
-        akkaWithStateActor <- AkkaWithStateTest.makeActor sys
-        akklingActor <- AkklingTest.makeActor sys
-        akklingWithStateActor <- AkklingWithStateTest.makeActor sys
-        wakkaActor <- WAkkaTest.makeActor sys
-        wakkaWhileActor <- WAkkaWhileTest.makeActor sys
-        wakkaSmActor <- WAkkaSmTest.makeActor sys
         
     [<BenchmarkDotNet.Attributes.Benchmark(Baseline = true)>]
     member _.Akka () =
-        runTest akkaActor
+        startStop.start.SetResult()
+        startStop.stop.Task.Wait()
+
+    [<BenchmarkDotNet.Attributes.IterationSetup(Target = nameof Unchecked.defaultof<ActorBenchmarks>.Akka)>]
+    member _.AkkaSetup () =
+        startStop <- prepTest (AkkaTest.makeActor sys)
         
     [<BenchmarkDotNet.Attributes.Benchmark>]
     member _.AkkaWithState () =
-        runTest akkaWithStateActor
-        
+        startStop.start.SetResult()
+        startStop.stop.Task.Wait()
+
+    [<BenchmarkDotNet.Attributes.IterationSetup(Target = nameof Unchecked.defaultof<ActorBenchmarks>.AkkaWithState)>]
+    member _.AkkaWithStateSetup () =
+        startStop <- prepTest (AkkaWithStateTest.makeActor sys)
+            
     [<BenchmarkDotNet.Attributes.Benchmark>]
     member _.Akkling () =
-        runTest akklingActor
+        startStop.start.SetResult()
+        startStop.stop.Task.Wait()
         
+    [<BenchmarkDotNet.Attributes.IterationSetup(Target = nameof Unchecked.defaultof<ActorBenchmarks>.Akkling)>]
+    member _.AkklingSetup () =
+        startStop <- prepTest (AkklingTest.makeActor sys)
+
     [<BenchmarkDotNet.Attributes.Benchmark>]
     member _.AkklingWithState () =
-        runTest akklingWithStateActor
+        startStop.start.SetResult()
+        startStop.stop.Task.Wait()
         
+    [<BenchmarkDotNet.Attributes.IterationSetup(Target = nameof Unchecked.defaultof<ActorBenchmarks>.AkklingWithState)>]
+    member _.AkklingWithStateSetup () =
+        startStop <- prepTest (AkklingWithStateTest.makeActor sys)
+
     [<BenchmarkDotNet.Attributes.Benchmark>]
     member _.WAkka () =
-        runTest wakkaActor
+        startStop.start.SetResult()
+        startStop.stop.Task.Wait()
         
+    [<BenchmarkDotNet.Attributes.IterationSetup(Target = nameof Unchecked.defaultof<ActorBenchmarks>.WAkka)>]
+    member _.WAkkaSetup () =
+        startStop <- prepTest (WAkkaTest.makeActor sys)
+
     [<BenchmarkDotNet.Attributes.Benchmark>]
     member _.WAkkaWhile () =
-        runTest wakkaWhileActor
-        
+        startStop.start.SetResult()
+        startStop.stop.Task.Wait()
+
+    [<BenchmarkDotNet.Attributes.IterationSetup(Target = nameof Unchecked.defaultof<ActorBenchmarks>.WAkkaWhile)>]
+    member _.WAkkaWhileSetup () =
+        startStop <- prepTest (WAkkaWhileTest.makeActor sys)
+            
     [<BenchmarkDotNet.Attributes.Benchmark>]
     member _.WAkkaSm () =
-        runTest wakkaSmActor
+        startStop.start.SetResult()
+        startStop.stop.Task.Wait()
+
+    [<BenchmarkDotNet.Attributes.IterationSetup(Target = nameof Unchecked.defaultof<ActorBenchmarks>.WAkkaSm)>]
+    member _.WAkkaSmSetup () =
+        startStop <- prepTest (WAkkaSmTest.makeActor sys)
         
 let runNonBenchmarkTests () = 
-    let numMessages = args.GetResult(Args.NumMessages, 10000)
+    let numMessages = args.GetResult(Args.NumMessages, 1000000)
     let tests = args.GetResult(Args.Tests, ["all"])
     
     let sys = System.create "perf-test" (Configuration.defaultConfig ())
     
-    let runTest (mode: string) testActor =
+    let runTest (mode: string) (testActor: IActorRef<obj>) =
         if
             tests |> List.contains (mode.ToLower())
             || tests |> List.contains "all"
         then 
-            let tcs = System.Threading.Tasks.TaskCompletionSource<unit>()
+            let startTcs = System.Threading.Tasks.TaskCompletionSource<unit>()
+            let readyTcs = System.Threading.Tasks.TaskCompletionSource<unit>()
+            let stopTcs = System.Threading.Tasks.TaskCompletionSource<unit>()
             let msgs = [|
                 for i in 1..numMessages do
                     {index = i; stop = None}
-                {index = 0; stop = Some tcs}
+                {index = 0; stop = Some stopTcs}
             |]
-            let start = System.DateTime.Now
+            testActor <! {start = startTcs; ready = readyTcs}
             for msg in msgs do
                 testActor <! msg
-            tcs.Task.Wait()
+            readyTcs.Task.Wait()
+            let start = System.DateTime.Now
+            startTcs.SetResult()
+            stopTcs.Task.Wait()
             let timeSpan = System.DateTime.Now - start
             let rate = float numMessages/timeSpan.TotalSeconds
             printfn $"{mode}: {rate} msgs/sec"
@@ -282,8 +365,8 @@ let runNonBenchmarkTests () =
     
     let akka = runTest "Akka" (AkkaTest.makeActor sys)
     let akkaWithState = runTest "AkkaWithState" (AkkaWithStateTest.makeActor sys)
-    let akkling = runTest "Akkling" (AkklingTest.makeActor sys)
-    let akklingWithState = runTest "AkklingWithState" (AkklingWithStateTest.makeActor sys)
+    let akkling = runTest "Akkling" (AkklingTest.makeActor sys |> retype)
+    let akklingWithState = runTest "AkklingWithState" (AkklingWithStateTest.makeActor sys |> retype)
     let wakka = runTest "WAkka" (WAkkaTest.makeActor sys)
     let wakkaWhile = runTest "WAkkaWhile" (WAkkaWhileTest.makeActor sys)
     let wakkaSm = runTest "WAkkaSm" (WAkkaSmTest.makeActor sys)
